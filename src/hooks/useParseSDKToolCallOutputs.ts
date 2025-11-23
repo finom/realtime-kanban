ba7305f96210d902490d71e8d13a2e1d013d52cd
Andrey Gubanov <andrii@gubanov.eu>
2025-11-23 13:40:13 +0400

    feat: Create useParseSDKToolCallOutputs hook

---
+++ src/hooks/useParseSDKToolCallOutputs.ts
@@ -0,0 +1,33 @@
+import { useRegistry } from "@/registry";
+import { ToolUIPart, UIMessage } from "ai";
+import { useEffect, useRef } from "react";
+
+ 
+ export default function useParseSDKToolCallOutputs(
+  messages: UIMessage[],
+) {
+ const parsedToolCallIdsSetRef = useRef<Set<string>>(new Set());
+
+  useEffect(() => {
+    // useRegistry.getState().parse(messages); - this would also work, but would be less efficient
+    const partsToParse = messages.flatMap((msg) =>
+      msg.parts.filter((part) => {
+        return (
+          msg.role === "assistant" &&
+          part.type.startsWith("tool-") &&
+          (part as ToolUIPart).state === "output-available" &&
+          "toolCallId" in part &&
+          !parsedToolCallIdsSetRef.current.has(part.toolCallId)
+        );
+      }),
+    ) as ToolUIPart[];
+
+    partsToParse.forEach((part) =>
+      parsedToolCallIdsSetRef.current.add(part.toolCallId),
+    );
+
+    if(partsToParse.length) {
+      useRegistry.getState().parse(partsToParse.map((part) => part.output));
+    }
+  }, [messages]);
+}

009d60645e2c49ea1914009b2426e8f0b64275a8
Andrey Gubanov <andrii@gubanov.eu>
2026-04-03 15:16:42 +0400

    fix: Renew deps; remove workarounds on fetcher

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,19 +1,6 @@
 import { createFetcher, HttpStatus } from 'vovk';
 
-type OnSuccessHandler = (
-  data: unknown,
-  options: { bypassRegistry?: boolean },
-) => void | Promise<void>;
-
-// TODO: Replace with built-in fetcher.onSuccess once supported by Vovk
-const onSuccessHandlers: OnSuccessHandler[] = [];
-
-const _fetcher = createFetcher<{ bypassRegistry?: boolean }>({
-  onSuccess: async (data, options) => {
-    for (const handler of onSuccessHandlers) {
-      handler(data, options);
-    }
-  },
+export const fetcher = createFetcher<{ bypassRegistry?: boolean }>({
   onError: (error) => {
     if (error.statusCode === HttpStatus.UNAUTHORIZED) {
       document.location.href = '/login';
@@ -21,8 +8,3 @@ const _fetcher = createFetcher<{ bypassRegistry?: boolean }>({
   },
 });
 
-export const fetcher = Object.assign(_fetcher, {
-  onSuccess: (handler: OnSuccessHandler) => {
-    onSuccessHandlers.push(handler);
-  },
-});
